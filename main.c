72014a897e0bfa3c2e18c514a7f29aeeb1bccab5
lucianoMintrone <lucianomintrone@gmail.com>
2018-10-19 00:35:06 -0300

    Add init function

--- main.c
+++ main.c
@@ -6,11 +6,14 @@
 #include <string.h>
 #include <time.h>
 
+typedef unsigned char byte;
+
 typedef struct Block {
 	bool is_dirty;
+	bool is_valid:
 	time_t last_used_at;
 	int tag;
-	char bytes[6];
+	byte bytes[64];
 } Block;
 
 typedef struct Set {
@@ -20,24 +23,52 @@ typedef struct Set {
 typedef struct Cache {
 	int number_of_memory_accesses;
 	int number_of_misses;
-	Set sets[1024];
+	Set sets[16];
 } Cache;
 
 int get_offset(int address) {
-	int bit_mask = (1 << 0) | (1 << 1) | (1 << 2) | (1 << 3);
+	int bit_mask = (1 << 0) | (1 << 1) | (1 << 2) | (1 << 3) | (1 << 4) | (1 << 5);
 	return (byte)(address & bit_mask);
 }
 
 int get_index(int address) {
-	int bit_mask = (1 << 4) | (1 << 5) | (1 << 6) | (1 << 7);
+	address = address >> 6;
+	int bit_mask = (1 << 0) | (1 << 1) | (1 << 2) | (1 << 3);
 	return (byte)(address & bit_mask);
 }
 
 int get_tag(int address) {
-	int bit_mask = (1 << 8) | (1 << 9) | (1 << 10) | (1 << 11) | (1 << 12) | (1 << 13) | (1 << 14) | (1 << 15);
+	address = address >> 10;
+	int bit_mask = (1 << 0) | (1 << 1) | (1 << 2) | (1 << 3) | (1 << 4) | (1 << 5);
 	return (byte)(address & bit_mask);
 }
 
+Cache cache;
+
+void init() {
+	cache.number_of_misses = 0;
+	cache.number_of_memory_accesses = 0;
+	int i;
+	for (i = 0; i < 16; i++) {
+   		Set set;
+   		int j;
+   		for(j = 0; j < 4; j++) {
+   			Block block;
+   			block.is_dirty = false;
+   			block.is_valid = false;
+   			block.last_used_at = 0;
+   			block.tag = 0;
+   			int k;
+   			for(k = 0; k < 64; k++) {
+   				block.bytes[k] = 0;
+   			}
+   			set.blocks[j] = block;
+   		}
+   		cache.sets[i] = set;
+	}
+}
+
 int main (int argc, char *argv[]) {
+	init();
 	return EXIT_SUCCESS;
 }
